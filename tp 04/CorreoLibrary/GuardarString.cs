b2f2b6615ef1c12e317be05f71c2900760b69217
Shilua <shilua4@gmail.com>
2018-06-22 09:22:16 -0300

    TP 04

---
+++ tp 04/CorreoLibrary/GuardarString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class GuardarString
+    {
+        public static bool Guardar(this string texto, string archivo)
+        {
+            try
+            {
+                
+                using (StreamWriter guardar = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + archivo + ".txt", true))
+                {
+                    guardar.WriteLine(texto);
+                    guardar.Close();
+                }
+
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tp 04/CorreoLibrary/PaqueteDAO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Entidades
+{
+    public class PaqueteDAO
+    {
+        #region Fields
+        private static SqlCommand comando;
+        private static SqlConnection conexion;
+        #endregion
+
+        #region Methods
+
+        public PaqueteDAO()
+        {
+            PaqueteDAO.conexion = new SqlConnection(Properties.Settings.Default.cadenaConecction/*"Data Source=DESKTOP-QRH6RB9\\SQLEXPRESS;Initial Catalog=correo-sp-2017;Integrated Security=True"*/);
+            PaqueteDAO.comando = new SqlCommand();
+
+            PaqueteDAO.comando.CommandType = System.Data.CommandType.Text;
+            PaqueteDAO.comando.Connection = PaqueteDAO.conexion;
+        }
+
+        public static bool Insertar(Paquete p)
+        {
+            PaqueteDAO dAO = new PaqueteDAO();
+            bool retorno = false;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("INSERT INTO dbo.Paquetes (direccionEntrega,trackingID,alumno) VALUES('{0}','{1}','{2}')", p.DireccionEntrega, p.TrackingID, "Vargas Maximiliano");
+            try
+            {
+                retorno = EjecutarNonQuery(sb.ToString());
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+                return retorno;
+        }
+
+        /// <summary>
+        /// Funcion que ejecuta el non query
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        private static bool EjecutarNonQuery(string sql)
+        {
+            bool todoOk = false;
+            try
+            {
+                // LE PASO LA INSTRUCCION SQL
+                PaqueteDAO.comando.CommandText = sql;
+
+
+                // ABRO LA CONEXION A LA BD
+                PaqueteDAO.conexion.Open();
+
+                // EJECUTO EL COMMAND
+                PaqueteDAO.comando.ExecuteNonQuery();
+
+                todoOk = true;
+            }
+            catch (Exception e)
+            {
+                todoOk = false;
+                throw e;
+            }
+            finally
+            {
+                if (todoOk)
+                    PaqueteDAO.conexion.Close();
+            }
+            return todoOk;
+        }
+
+        #endregion
+    }
+}
